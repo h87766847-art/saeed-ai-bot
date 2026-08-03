c2c08095c80356603c4354503322fea33cadf831
h87766847-art <h87766847@gmail.com>
2026-08-03 20:53:18 +0330

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,9 +1,26 @@
 import datetime
 
+from goals import (
+    init_goals,
+    add_goal,
+    get_goals
+)
 
-# =========================
-# ابزار زمان
-# =========================
+from habits import (
+    init_habits,
+    add_habit,
+    complete_habit,
+    show_habits
+)
+
+
+init_goals()
+init_habits()
+
+
+NOTES_FILE = "saeed_notes.txt"
+
+
 
 def get_time():
 
@@ -14,14 +31,6 @@ def get_time():
         +
         now.strftime("%Y-%m-%d %H:%M:%S")
     )
-
-
-
-# =========================
-# ابزار یادداشت ساده
-# =========================
-
-NOTES_FILE = "saeed_notes.txt"
 
 
 
@@ -54,21 +63,75 @@ def read_notes():
 
             return file.read()
 
-
     except:
 
-        return "هنوز یادداشتی وجود ندارد."
+        return "یادداشتی وجود ندارد."
 
 
 
-# =========================
-# تشخیص ابزار
-# =========================
+# =====================
+# Goals
+# =====================
+
+def save_goal(goal):
+
+    return add_goal(goal)
+
+
+
+def show_goal_list():
+
+    goals = get_goals()
+
+
+    if not goals:
+
+        return "هدف ثبت نشده."
+
+
+    result = "هدف‌های حسین:\n"
+
+
+    for title,status,progress in goals:
+
+        result += (
+            f"- {title} | "
+            f"{progress}%\n"
+        )
+
+
+    return result
+
+
+
+# =====================
+# Habits
+# =====================
+
+def save_habit(habit):
+
+    return add_habit(
+        habit
+    )
+
+
+
+def complete_task(task):
+
+    return complete_habit(
+        task
+    )
+
+
+
+# =====================
+# Tool Router
+# =====================
 
 def use_tool(text):
 
 
-    if "ساعت" in text or "زمان" in text:
+    if "زمان" in text or "ساعت" in text:
 
         return get_time()
 
@@ -90,6 +153,70 @@ def use_tool(text):
     if "یادداشت ها" in text:
 
         return read_notes()
+
+
+
+    if "هدفم" in text or "هدف من" in text:
+
+        goal = text.replace(
+            "هدفم",
+            ""
+        )
+
+        goal = goal.replace(
+            "هدف من",
+            ""
+        )
+
+
+        return save_goal(
+            goal.strip()
+        )
+
+
+
+    if "هدف ها" in text or "هدف‌ها" in text:
+
+        return show_goal_list()
+
+
+
+    if "ماموریت" in text or "کار امروز" in text:
+
+        habit = text.replace(
+            "ماموریت",
+            ""
+        )
+
+        habit = habit.replace(
+            "کار امروز",
+            ""
+        )
+
+
+        return save_habit(
+            habit.strip()
+        )
+
+
+
+    if "انجام شد" in text:
+
+        task = text.replace(
+            "انجام شد",
+            ""
+        )
+
+
+        return complete_task(
+            task.strip()
+        )
+
+
+
+    if "ماموریت ها" in text or "ماموریت‌ها" in text:
+
+        return show_habits()
 
 
 

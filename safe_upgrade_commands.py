750201a5aec05f8a728f63578a90b86aa6c49713
h87766847-art <h87766847@gmail.com>
2026-08-04 18:09:22 +0330

    Create safe_upgrade_commands.py

---
+++ safe_upgrade_commands.py
@@ -0,0 +1,30 @@
+# safe_upgrade_commands.py
+# Saeed Core v9.9
+# Safe Upgrade Telegram Commands
+
+
+from safe_upgrade_engine import (
+    run_safe_upgrade,
+    safe_upgrade_status
+)
+
+
+
+
+
+
+def command_safe_upgrade_start():
+
+
+    return run_safe_upgrade()
+
+
+
+
+
+
+
+def command_safe_upgrade_status():
+
+
+    return safe_upgrade_status()

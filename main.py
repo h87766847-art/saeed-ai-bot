3ea1c84043c3e3121b82c84729569264d12bd704
h87766847-art <h87766847@gmail.com>
2026-08-06 01:43:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({
         "status": "online",
-        "message": "AI Bot is running 🚀"
+        "message": "Saeed AI Bot is running"
     })
+
 
 @app.route("/health")
 def health():
     return jsonify({
         "ok": True
     })
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
